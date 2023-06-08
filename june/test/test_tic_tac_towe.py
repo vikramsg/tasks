2c8989221230f8d3e900b2aed62ba148b40be841
Vikram <vikramsingh001@gmail.com>
2023-06-08 15:05:07 +0200

    Add failing test

--- june/test/test_tic_tac_towe.py
+++ june/test/test_tic_tac_towe.py
@@ -4,17 +4,29 @@ import pytest
 from click import testing as click_testing
 
 
-def test_game_play(game_moves: List[str]) -> None:
+@pytest.mark.parametrize(
+    "moves, expected_msg",
+    [
+        (
+            ["o", "1,1", "0,0", "1,0", "2,1", "2,2", "0,2", "0,1", "1,2", "2,0"],
+            "Game finished in a draw",
+        ),
+        (
+            ["x", "0,0", "1,1", "1,2", "1,0", "0,1", "0,2", "2,1", "2,0"],
+            "Game over. Player o won.",
+        ),
+        (
+            ["o", "2,1", "1,0", "2,2", "0,1", "2,0"],
+            "Game over. Player x won.",
+        ),
+    ],
+)
+def test_game_play(moves: List[str], expected_msg) -> None:
     # Given
     test_runner = click_testing.CliRunner()
 
     # When
-    result = test_runner.invoke(game_loop, input="\n".join(game_moves))
+    result = test_runner.invoke(game_loop, input="\n".join(moves))
 
     # Then
-    assert "Game finished in a draw" in result.output
-
-
-@pytest.fixture
-def game_moves() -> List[str]:
-    return ["o", "1,1", "0,0", "1,0", "2,1", "2,2", "0,2", "0,1", "1,2", "2,0"]
+    assert expected_msg in result.output
